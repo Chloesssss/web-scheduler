c12db0dbd3f3f551553b138bdf802d9ec050d382
lipengpeng <1656721094@qq.com>
2022-03-04 15:37:09 +0800

    更新接口代理和项目名称

--- vue.config.js
+++ vue.config.js
@@ -31,12 +31,12 @@ module.exports = {
       //   },
       // },
       "/dolphinscheduler": {
-        target: "http://192.168.25.22:12345/", // 后台接口域名
+        target: "http://192.168.25.22:12345/dolphinscheduler", // 后台接口域名
         ws: false, //如果要代理 websockets，配置这个参数
         secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin: true, //是否跨域
         pathRewrite: {
-          "^/api": "",
+          "^/dolphinscheduler": "",
         },
       },
     },
