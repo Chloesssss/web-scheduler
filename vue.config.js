0184bd47889fb5a9faaf39c5eba3361ae22592e6
Chloe <532357569@qq.com>
2022-06-09 17:10:18 +0800

    21部署

--- vue.config.js
+++ vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     proxy: {
       "/api": {
-        target: "http://192.168.25.20:10001/", // 后台接口域名
+        target: "http://192.168.25.21:10001/", // 后台接口域名
         ws: false, //如果要代理 websockets，配置这个参数
         secure: false, // 如果是https接口，需要配置这个参数
         changeOrigin: true, //是否跨域
